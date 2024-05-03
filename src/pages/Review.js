8592154455a2fbf2a62dfd092c5ac8cebd1f9162
jiwonee02 <nepthun@naver.com>
2024-05-04 02:20:23 +0900

    ✨ Feat : 후기 작성 및 후기 리스트 (#2)
    후기 작성 및 후기 리스트 구현

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -1,36 +1,123 @@
 import React from "react";
+import { useState, useEffect, useRef } from "react";
 import * as C from "../styles/CommonStyle";
 import * as R from "../styles/ReviewStyle";
 
 import PcTitle from "../components/PcTitle";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { UNSAFE_DataRouterStateContext } from "react-router-dom";
+import cloudLeft from "../img/3_cloud_104x75.png";
+import cloudRight from "../img/3_cloud_131x144.png";
+import parasol from "../img/parasol&tube_76x47.png";
 
 function Review() {
-  return (
-    <>
-      <C.Page>
-        <C.BlackBg />
-        <C.Area>
-          <C.Title>
-            <PcTitle />
-          </C.Title>
-          <R.Background>
-            <C.Phone>
-              <R.Review>
-                <Header />
-                <C.PageTitle>REVIEW</C.PageTitle>
-                여기 부분 작업!
-                <br />
-                기존 코드 절대 수정하면 안 됩니다
-                {/* <Footer /> */}
-              </R.Review>
-            </C.Phone>
-          </R.Background>
-        </C.Area>
-      </C.Page>
-    </>
-  );
+    const [text, setText] = useState("");
+    const [review, setReview] = useState([]);
+    const writeRef = useRef();
+    const onChange = (event) => {
+        const height = event.target.value;
+        setText(height);
+    };
+    const checkTime = (time) => {
+        const currentTime = new Date();
+        const submitTime = new Date(time);
+        const timeDifference = (currentTime - submitTime) / 1000;
+        if (timeDifference < 60) {
+            return "방금";
+        } else if (timeDifference < 3600) {
+            const minutes = Math.floor(timeDifference / 60);
+            return `${minutes}분 전`;
+        } else if (timeDifference < 86400) {
+            const hours = Math.floor(timeDifference / 3600);
+            return `${hours}시간 전`;
+        } else {
+            const options = {
+                month: "2-digit",
+                day: "2-digit",
+            };
+            return submitTime.toLocaleString("en-US", options);
+        }
+    };
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (text === "") {
+            return;
+        }
+        const currentTime = new Date();
+        setReview((currentArray) => [
+            { text, time: currentTime },
+            ...currentArray,
+        ]);
+        setText("");
+    };
+    useEffect(() => {
+        writeRef.current.style.height = "76px";
+        const scrollHeight = writeRef.current.scrollHeight;
+        writeRef.current.style.height = scrollHeight + "px";
+    }, [text]);
+
+    return (
+        <>
+            <C.Page>
+                <C.BlackBg />
+                <C.Area>
+                    <C.Title>
+                        <PcTitle />
+                    </C.Title>
+                    <R.Background>
+                        <C.Phone>
+                            <R.Review>
+                                <Header />
+                                <C.PageTitle>REVIEW</C.PageTitle>
+                                <R.ReviewWrite>
+                                    <R.CloudLeft>
+                                        <img src={cloudLeft} alt="" />
+                                    </R.CloudLeft>
+                                    <R.CloudRight>
+                                        <img src={cloudRight} alt="" />
+                                    </R.CloudRight>
+                                    <R.Write onSubmit={onSubmit}>
+                                        <R.WriteText
+                                            ref={writeRef}
+                                            onChange={onChange}
+                                            value={text}
+                                            type="text"
+                                            placeholder="2024 근화제 <찬란> 재밌게 즐기셨나요?
+                                            후기를 남겨주세요!"
+                                        ></R.WriteText>
+                                        <R.WriteButton hasText={text}>
+                                            등록하기
+                                        </R.WriteButton>
+                                    </R.Write>
+                                </R.ReviewWrite>
+                                <R.ReviewList>
+                                    <R.List>
+                                        {review.length !== 0 && (
+                                            <R.Parasol>
+                                                <img src={parasol} alt="" />
+                                            </R.Parasol>
+                                        )}
+                                        {review.map((item, index) => (
+                                            <R.ListItem key={index}>
+                                                <R.ListText>
+                                                    {item.text}
+                                                </R.ListText>
+                                                <R.ListTime>
+                                                    {checkTime(item.time)}
+                                                </R.ListTime>
+                                            </R.ListItem>
+                                        ))}
+                                    </R.List>
+                                </R.ReviewList>
+                                {/* <Footer /> */}
+                            </R.Review>
+                        </C.Phone>
+                    </R.Background>
+                </C.Area>
+            </C.Page>
+        </>
+    );
 }
 
 export default Review;
--- src/styles/ReviewStyle.js
+++ src/styles/ReviewStyle.js
@@ -1,19 +1,125 @@
 import styled from "styled-components";
 
 export const Background = styled.div`
-  background: linear-gradient(to bottom, #3177c6 6%, #83beff 46%, #ffffff 94%);
+    background: linear-gradient(
+        to bottom,
+        #3177c6 6%,
+        #83beff 46%,
+        #ffffff 94%
+    );
 
-  @media (hover: hover) and (pointer: fine) and (min-height: 801px) {
-    border-radius: 12px;
-  }
+    @media (hover: hover) and (pointer: fine) and (min-height: 801px) {
+        border-radius: 12px;
+    }
 `;
 
 export const Review = styled.div`
-  width: 100vw;
-  height: 100vh;
+    width: 100vw;
+    height: 100vh;
 
-  @media (hover: hover) and (pointer: fine) {
-    width: 100%;
-    height: 100%;
-  }
+    @media (hover: hover) and (pointer: fine) {
+        width: 100%;
+        height: 100%;
+    }
+`;
+
+export const ReviewWrite = styled.div`
+    margin-top: 28px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const CloudLeft = styled.div`
+    width: 102px;
+    height: 74px;
+    position: absolute;
+    top: 6.5rem;
+    left: -1rem;
+`;
+
+export const CloudRight = styled.div`
+    width: 143px;
+    height: 144px;
+    position: absolute;
+    top: 12rem;
+    right: -0.7rem;
+    z-index: -1;
+`;
+
+export const Write = styled.form``;
+
+export const WriteText = styled.textarea`
+    display: flex;
+    box-sizing: border-box;
+    padding: 18px 20px;
+    width: 328px;
+    border-radius: 16px;
+    border: none;
+    font-size: 15px;
+    min-height: 76px;
+    resize: none;
+    color: #ffffff;
+    background-color: rgba(255, 255, 255, 0.5);
+    overflow: hidden;
+    &::placeholder {
+        color: rgba(255, 255, 255, 0.5);
+        white-space: pre-wrap;
+    }
+`;
+
+export const WriteButton = styled.button`
+    display: flex;
+    margin: 12px 16px;
+    margin-left: auto;
+    cursor: pointer;
+    border: none;
+    font-size: 15px;
+    background-color: transparent;
+    color: ${(props) =>
+        props.hasText ? "#ffffff;" : "rgba(255, 255, 255, 0.5);"};
+`;
+
+export const ReviewList = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const List = styled.ul`
+    list-style: none;
+    padding: 0;
+    margin: 0;
+`;
+
+export const Parasol = styled.div`
+    width: 46px;
+    height: 46px;
+    /*display: ${({ isVisible }) => (isVisible ? "block" : "none")};*/
+`;
+
+export const ListItem = styled.li`
+    flex-direction: column;
+    box-sizing: border-box;
+    margin: 0px 0px 16px 0px;
+    display: felx;
+    min-height: 74px;
+    width: 328px;
+    border-radius: 16px;
+    border: none;
+    background-color: #ffffff;
+    padding: 16px 16px 12px 16px;
+`;
+
+export const ListText = styled.div`
+    display: felx;
+    color: #7b7b7b;
+    font-size: 15px;
+    white-space: pre-wrap;
+`;
+export const ListTime = styled.div`
+    display: felx;
+    color: #7b7b7b;
+    font-size: 12px;
+    margin-top: 12px;
 `;
