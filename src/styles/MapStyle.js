8ca228c39684485a59fe01c83af1dcd6417a8b47
sleepy2535 <greenapple5657@gmail.com>
2024-05-10 21:35:05 +0900

    💄 지도 추가 디자인 완성
    지도 추가 디자인 완성했는데 이미지 부분은 추후에 받으면 변경할 예정

--- src/styles/MapStyle.js
+++ src/styles/MapStyle.js
@@ -1,19 +1,81 @@
 import styled from "styled-components";
 
 export const Background = styled.div`
-  background: linear-gradient(to bottom, #2774cc 6%, #499ffc 46%, #ffffff 94%);
+    background: linear-gradient(
+        to bottom,
+        #2774cc 6%,
+        #499ffc 46%,
+        #ffffff 94%
+    );
 
-  @media (hover: hover) and (pointer: fine) and (min-height: 801px) {
-    border-radius: 12px;
-  }
+    @media (hover: hover) and (pointer: fine) and (min-height: 801px) {
+        border-radius: 12px;
+    }
 `;
 
 export const Map = styled.div`
-  width: 100vw;
-  height: 100vh;
+    width: 100vw;
+    height: 100vh;
 
-  @media (hover: hover) and (pointer: fine) {
-    width: 100%;
-    height: 100%;
-  }
+    @media (hover: hover) and (pointer: fine) {
+        width: 100%;
+        height: 100%;
+    }
+`;
+
+export const Mapimg = styled.img`
+    width: 200px;
+    height: 300px;
+`;
+
+export const Mapimgs = styled.div`
+    display: flex;
+    justify-content: center;
+    object-fit: contain;
+    margin-top: 10%;
+`;
+
+export const MapTitle = styled.div`
+    &:after {
+        content: " ";
+        flex-grow: 1;
+        margin: 0px 16px;
+        background: rgba(255, 255, 255, 0.35);
+        height: 1px;
+        font-size: 0px;
+        line-height: 0px;
+    }
+    &:before {
+        content: " ";
+        flex-grow: 1;
+        margin: 0px 16px;
+        background: rgba(255, 255, 255, 0.35);
+        height: 1px;
+        font-size: 0px;
+        line-height: 0px;
+    }
+    display: flex;
+    flex-basis: 100%;
+    align-items: center;
+    color: rgba(255, 255, 255, 0.35);
+    font-size: 14px;
+    margin: 8px 0px;
+`;
+
+export const Mapline = styled.div`
+    width: 90%;
+    height: 2px;
+    font-size: 14px;
+    margin: 8px auto; /* 가운데 정렬 */
+    background: rgba(255, 255, 255, 0.35);
+    margin-top: 8px; /* 줄 선 위쪽 여백 조정 */
+    margin-bottom: 8px; /* 줄 선 아래쪽 여백 조정 */
+`;
+
+export const MapInfo = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 10px;
+    margin-top: 8%;
+    flex-direction: column;
 `;
