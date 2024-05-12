1b4ac534b8eeb4c9575c1c3379517cba0761f6f5
Yunji-Yun <dbswlvkdlxld2022@naver.com>
2024-05-13 00:58:49 +0900

    🐛 Fix : 반응형 왼쪽 밀림 문제 해결
    오른쪽 상단 고정 햇빛이 프레임 밀린 부분에 적용이 되어서 그만큼 다시 밀어서 해결

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,29 +4,29 @@ import * as M from "../styles/MainStyle";
 
 import PcTitle from "../components/PcTitle";
 import Footer from "../components/Footer";
-import Shortcut from '../components/Shortcut';
+import Shortcut from "../components/Shortcut";
 
 function Main() {
-  return (
-    <>
-      <C.Page>
-        <C.BlackBg />
-        <C.Area>
-          <C.Title>
-            <PcTitle />
-          </C.Title>
-          <M.Background>
-            <C.Phone>
-              <M.Main>
-                <Shortcut/>
-                <Footer/>
-              </M.Main>
-            </C.Phone>
-          </M.Background>
-        </C.Area>
-      </C.Page>
-    </>
-  );
+    return (
+        <>
+            <C.Page>
+                <C.BlackBg />
+                <C.Area>
+                    <C.Title>
+                        <PcTitle />
+                    </C.Title>
+                    <M.Background>
+                        <C.Phone>
+                            <M.Main>
+                                <Shortcut />
+                                <Footer />
+                            </M.Main>
+                        </C.Phone>
+                    </M.Background>
+                </C.Area>
+            </C.Page>
+        </>
+    );
 }
 
 export default Main;
--- src/styles/MainStyle.js
+++ src/styles/MainStyle.js
@@ -1,19 +1,24 @@
 import styled from "styled-components";
 
 export const Background = styled.div`
-  background: linear-gradient(to bottom, #3177c6 6%, #83beff 46%, #ffffff 94%);
+    background: linear-gradient(
+        to bottom,
+        #3177c6 6%,
+        #83beff 46%,
+        #ffffff 94%
+    );
 
-  @media (hover: hover) and (pointer: fine) and (min-height: 801px) {
-    border-radius: 12px;
-  }
+    @media (hover: hover) and (pointer: fine) and (min-height: 801px) {
+        border-radius: 12px;
+    }
 `;
 
 export const Main = styled.div`
-  width: 100vw;
-  height: 100vh;
+    width: 100vw;
+    height: 100vh;
 
-  @media (hover: hover) and (pointer: fine) {
-    width: 100%;
-    height: 100%;
-  }
+    @media (hover: hover) and (pointer: fine) {
+        width: 100%;
+        height: 100%;
+    }
 `;
