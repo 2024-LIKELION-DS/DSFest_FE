a1b899713fee6893ee7c16f58a1f46c48364369d
pookey1104 <pookey1104@naver.com>
2024-05-20 14:13:08 +0900

    🐛 Fix : main 푸터 문제 해결
    main 푸터 문제 해결

--- src/styles/MainStyle.js
+++ src/styles/MainStyle.js
@@ -1,16 +1,11 @@
 import styled from "styled-components";
 
 export const Background = styled.div`
-    background: linear-gradient(
-        to bottom,
-        #3177c6 6%,
-        #83beff 46%,
-        #ffffff 94%
-    );
+  background: linear-gradient(to bottom, #3177c6 6%, #83beff 46%, #ffffff 94%);
 
-    @media (hover: hover) and (pointer: fine) and (min-height: 801px) {
-        border-radius: 12px;
-    }
+  @media (hover: hover) and (pointer: fine) and (min-height: 801px) {
+    border-radius: 12px;
+  }
 `;
 
 export const Main = styled.div`
@@ -18,8 +13,8 @@ export const Main = styled.div`
   min-height: calc(100vh - 183px);
   position: relative;
 
-    @media (hover: hover) and (pointer: fine) {
-        width: 100%;
-        height: 100%;
-    }
+  @media (hover: hover) and (pointer: fine) {
+    width: 100%;
+    min-height: calc(100% - 183px);
+  }
 `;
